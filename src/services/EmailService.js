23b2d96cb10e5fe53cf6641b26a174e111cdba81
Richie Hujaya <richie.hujaya2007@gmail.com>
2026-07-02 00:21:52 +0700

    fix email railway

--- src/services/EmailService.js
+++ src/services/EmailService.js
@@ -7,7 +7,8 @@ class EmailService {
     this.transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: Number(process.env.SMTP_PORT),
-      secure: false, // true for 465, false for 587 (STARTTLS)
+      secure: false,
+      family: 4,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
